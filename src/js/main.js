2df1b949d4c3201de5f8307d7ea22d2984eed698
Analo-pez <ana.ts.lopez@gmail.com>
2020-07-04 18:45:01 +0200

    add nav with game

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('>> Ready :)');
+// selector
+var menu = document.querySelector('.hamburger');
+
+// method
+function toggleMenu(event) {
+    this.classList.toggle('is-active');
+    document.querySelector(".menuppal").classList.toggle("is_active");
+    event.preventDefault();
+}
+
+// event
+menu.addEventListener('click', toggleMenu, false);
+
