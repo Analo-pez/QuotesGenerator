2a8422d5609f1b2b16e8be90f0d3edbfa2dbe7f3
Analo-pez <ana.ts.lopez@gmail.com>
2020-07-08 12:37:10 +0200

    update functions and modify api

--- src/js/main.js
+++ src/js/main.js
@@ -12,38 +12,54 @@ function toggleMenu(event) {
 menu.addEventListener('click', toggleMenu, false);
 
 
-//QUOTE GENERATOR
+//QUOTE GENERATOR- GAME PAGE
 
+// SELECTORS
 const name = document.querySelector('.js-name');
 const button = document.querySelector('.js-button');
 const quote = document.querySelector('.js-quote');
 const thanks = document.querySelector('.js-thanks');
+let quoteArr = [];
 
 
-const arrayQuotes = ['La vida es algo más que salir a buscar castañas y acabar cogiendo higos en un árbol de manzanas', 'A veces, pensar demasiado, es quedar en sitio estanco', 'Antes de avanzar, acuérdate de mirar atrás', 'Tan deficiente es una casa sin tejado, como una base sin consolidar', 'Es hora de salir a comerse el mundo', 'No dejes para mañana lo que quieres en tu presente', 'Si pasas de largo, se te quedaba corto', 'No esperes que te lo digan, sabes que quieres hacerlo', 'La motivación se encuentra a la vuelta de una esquina por descubrir', 'No encuentras certeza, si no dejas hueco para que entre la duda', 'No pierdas el tiempo en ganarlo todo', 'Si te conformas con menos, pierdes de más', 'No tengas prisa, disfruta de todo el recorrido'];
+//INCLUIR SERVIDOR DE FRASES hacer un then que retorne el arrayQuotes
+const getApi = () => {
+	fetch("./api/quotes.json")
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			console.log(data);
+			quoteArr = data;//declarada arriba
+			paintQuote(quoteArr);//llamo aquí =>() por asincronía
+		})
+};
+
+getApi();
 
 
-// Lo que quiero hacer es: que el boton de generar frase no se active si el input name no está relleno (HECHO)
-// que cuando se pulse el boton pasen tres cosas:
-//  - se genere una frase Random del arrayQuotes (HECHO)
-//  - remove classList hidden al párrafo para que se pinte ahí (HECHO)
-//  - el nombre del input se guarde en la constante para usar en thanks (HECHO)
-
-//MAS COSAS PARA PULIR
-//  - index 13 me da undefined, quizá í-1
-//  - guardar un array como API??
-//  - refrescar número al generar frase, si no se refresca la página no cambia
-
-fetch("./api/quotes.json");
-
-function getAQuote(event) {
-	event.preventDefault();
-	pushGenerator();
-	handelText();
-	updateQuote();
-	getRandomNumber();
+//GENERAR NUMERO ALEATORIO
+function getRandomNumber(max) {
+	return Math.floor(Math.random() * max);
 }
 
+
+// //PINTAR FRASE RANDOM
+function paintQuote() {
+	const number = getRandomNumber(quoteArr.length);
+	console.log(number);
+	quote.innerHTML = quoteArr[number];
+}
+
+
+// FUNCIÓN QUE RECOGE FUNCIONES
+function getAQuote(event) {
+	event.preventDefault();
+	getRandomNumber();
+	pushGenerator();
+}
+
+//PINTAR FRASE "GRACIAS"
 function pushGenerator(ev) {
 	const person = document.querySelector('.js-person');
 	const personValue = name.value;
@@ -55,25 +71,30 @@ function pushGenerator(ev) {
 }
 button.addEventListener('click', getAQuote);
 
+
+//EVENTO AL TERMINAR DE ESCRIBIR EL INPUT
 function handelText(ev) {
 	if (name.value === '') {
 		quote.classList.add('hidden');
 		thanks.classList.add('hidden');
 	}
 }
-
 name.addEventListener('keyup', handelText);
 
-//GENERAR NUMERO ALEATORIO
-function getRandomNumber(max) {
-	return Math.floor(Math.random() * max);
-}
-const number = getRandomNumber(arrayQuotes.length);
-console.log(number);
 
-//PINTAR FRASE
-function updateQuote() {
-	quote.innerHTML = arrayQuotes[number];
-}
-button.addEventListener('click', getAQuote);
 
+
+
+
+	//MAS COSAS PARA PULIR
+	//  - refrescar número al generar frase, si no se refresca la página no cambia
+
+		//PLANTEAMIENTO Y PARTES REALIZADAS
+
+// Lo que quiero hacer es: que el boton de generar frase no se active si el input name no está relleno (HECHO)
+// que cuando se pulse el boton pasen tres cosas:
+//  - se genere una frase Random del arrayQuotes (HECHO)
+//  - remove classList hidden al párrafo para que se pinte ahí (HECHO)
+//  - el nombre del input se guarde en la constante para usar en thanks (HECHO)
+//  - index 13 me da undefined (Math.floor != math.ceil) (HECHO)
+//  - guardar un array como API?? (HECHO)
